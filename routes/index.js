97c48ef4db3edb35affb1323274e9606520a73fb
tysonpace777 <32.tpace@gmail.com>
2025-11-26 11:48:45 -0700

    added routes to swagger

--- routes/index.js
+++ routes/index.js
@@ -2,5 +2,7 @@ const router = require('express').Router();
 const passport = require('passport');
 
 router.use('/', require('./swagger'));
+router.use('/users', require("./users"));
+router.use('/tasks', require("./tasks"));
 
 module.exports = router;
