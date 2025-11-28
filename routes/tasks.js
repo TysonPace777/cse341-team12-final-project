377e7badfd11645a64d360eb6e5802a13c8ffb26
bright-droid <ndu24006@byui.edu>
2025-11-28 13:53:36 +0100

    added the POST and PUT request functionalities for the Tasks collection

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,12 +1,13 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const utilities = require('../utilities/index');
+const utilities = require("../utilities/index");
 
-const taskController = require('../controllers/tasks');
+const taskController = require("../controllers/tasks");
 
-router.get('/:id', utilities.handleErrors(taskController.getSingle));
-router.get('/', utilities.handleErrors(taskController.getAll));
-
-router.delete('/:id', utilities.handleErrors(taskController.deleteTask));
+router.get("/", utilities.handleErrors(taskController.getAll));
+router.get("/:id", utilities.handleErrors(taskController.getSingle));
+router.post("/", utilities.handleErrors(taskController.createTask));
+router.put("/:id", utilities.handleErrors(taskController.updateTask));
+router.delete("/:id", utilities.handleErrors(taskController.deleteTask));
 
 module.exports = router;
