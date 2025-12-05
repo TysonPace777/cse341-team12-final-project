7cbd414a71b3cb0bdd16bf1e1d68d63f0f983b7f
tysonpace777 <32.tpace@gmail.com>
2025-12-05 11:56:02 -0700

    Added all CRUD and swagger docs.

--- routes/goals.js
+++ routes/goals.js
@@ -1,17 +1,17 @@
 const express = require("express");
 const router = express.Router();
+const utilities = require('../utilities/index');
 
 const goalController = require("../controllers/goals");
-const {isAuthenticated} = require('../middleware/auth')
 
-router.get("/", goalController.getAll);
+router.get("/", utilities.handleErrors(goalController.getAll));
 
-router.get("/:id", goalController.getSingle);
+router.get("/:id", utilities.handleErrors(goalController.getSingle));
 
-router.post("/", isAuthenticated, goalController.createGoal);
+router.post("/", utilities.handleErrors(goalController.createGoal));
 
-router.put("/:id", isAuthenticated, goalController.updateGoal);
+router.put("/:id", utilities.handleErrors(goalController.updateGoal));
 
-router.delete("/:id", isAuthenticated, goalController.deleteGoal);
+router.delete("/:id", utilities.handleErrors(goalController.deleteGoal));
 
 module.exports = router;
