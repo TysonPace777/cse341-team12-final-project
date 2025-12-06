b46881838b4b452e4c39e9a09325b9d0488c604f
tysonpace777 <32.tpace@gmail.com>
2025-12-06 16:24:15 -0700

    validation and swagger should be functioning

--- validation/goals.js
+++ validation/goals.js
@@ -1,15 +1,8 @@
 const { body, validationResult } = require("express-validator");
 
-const goalValidationRules = () => [
-  body("what")
-    .isString()
-    .notEmpty()
-    .withMessage("what must be a string and not empty"),
-
-  body("time")
-    .isString()
-    .notEmpty()
-    .withMessage("time must be a string and not empty")
+const goalValidationRules = [
+  body("what").isString().notEmpty().withMessage("what must be a string and not empty"),
+  body("time").isString().notEmpty().withMessage("time must be a string and not empty")
 ];
 
 function validateGoal(req, res, next) {
--- validation/users.js
+++ validation/users.js
@@ -1,37 +1,13 @@
 const { body, validationResult } = require("express-validator");
 
-const userValidationRules = () => [
-  body("firstName")
-    .isString()
-    .notEmpty()
-    .withMessage("firstName must be a string and not empty"),
-
-  body("lastName")
-    .isString()
-    .notEmpty()
-    .withMessage("lastName must be a string and not empty"),
-
-  body("location")
-    .isString()
-    .notEmpty()
-    .withMessage("location must be a string and not empty"),
-
-  body("age")
-    .isInt()
-    .withMessage("age must be a number"),
-
-  body("language")
-    .isString()
-    .notEmpty()
-    .withMessage("language must be a string and not empty"),
-
-  body("birthMonth")
-    .isInt({ min: 1, max: 12 })
-    .withMessage("birthMonth must be a number between 1 and 12"),
-
-  body("birthYear")
-    .isInt()
-    .withMessage("birthYear must be a number")
+const userValidationRules = [
+  body("firstName").isString().notEmpty().withMessage("firstName must be a string and not empty"),
+  body("lastName").isString().notEmpty().withMessage("lastName must be a string and not empty"),
+  body("location").isString().notEmpty().withMessage("location must be a string and not empty"),
+  body("age").isInt().withMessage("age must be a number"),
+  body("language").isString().notEmpty().withMessage("language must be a string and not empty"),
+  body("birthMonth").isInt({ min: 1, max: 12 }).withMessage("birthMonth must be a number between 1 and 12"),
+  body("birthYear").isInt().withMessage("birthYear must be a number")
 ];
 
 function validateUser(req, res, next) {
