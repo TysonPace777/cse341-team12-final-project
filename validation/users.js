f57b9c1106f13d25bd98fe1dd43ac73993bfdf72
unknown <debbieokon32@gmail.com>
2025-12-07 15:09:49 +0100

    Updated the schemas for PUT and POST users collection

--- validation/users.js
+++ validation/users.js
@@ -1,13 +1,9 @@
 const { body, validationResult } = require("express-validator");
 
 const userValidationRules = [
-  body("firstName").isString().notEmpty().withMessage("firstName must be a string and not empty"),
-  body("lastName").isString().notEmpty().withMessage("lastName must be a string and not empty"),
-  body("location").isString().notEmpty().withMessage("location must be a string and not empty"),
-  body("age").isInt().withMessage("age must be a number"),
-  body("language").isString().notEmpty().withMessage("language must be a string and not empty"),
-  body("birthMonth").isInt({ min: 1, max: 12 }).withMessage("birthMonth must be a number between 1 and 12"),
-  body("birthYear").isInt().withMessage("birthYear must be a number")
+  body("username").isString().notEmpty().withMessage("username must be a string and not empty"),
+  body("displayName").isString().notEmpty().withMessage("displayName must be a string and not empty"),
+  body("profileUrl").isString().notEmpty().withMessage("profileUrl must be a string and not empty"),
 ];
 
 function validateUser(req, res, next) {
