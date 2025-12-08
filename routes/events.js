40d246178f85928ca4434475202b19b730bce2e4
angie <angiewheeler99@gmail.com>
2025-12-07 21:57:56 -0700

    updating tasks to 7 fields per requirement for 7.

--- routes/events.js
+++ routes/events.js
@@ -4,13 +4,14 @@ const utilities = require("../utilities/index");
 
 const eventController = require("../controllers/events");
 const { eventValidationRules, validateEvent } = require('../validation/events');
+const { isAuthenticated } = require("../middleware/auth");
 
 router.get("/", utilities.handleErrors(eventController.getAll));
 
 router.get("/:id", utilities.handleErrors(eventController.getSingle));
 
-router.post("/", eventValidationRules, validateEvent, utilities.handleErrors(eventController.createEvent));
-router.put("/:id", eventValidationRules, validateEvent, utilities.handleErrors(eventController.updateEvent));
+router.post("/", isAuthenticated,eventValidationRules, validateEvent, utilities.handleErrors(eventController.createEvent));
+router.put("/:id", isAuthenticated,eventValidationRules, validateEvent, utilities.handleErrors(eventController.updateEvent));
 
 router.delete("/:id", utilities.handleErrors(eventController.deleteEvent));
 
